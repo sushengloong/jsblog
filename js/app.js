68f2e29915547e1ee30ce807276982217921d6a0
Sheng-Loong Su <sushengloong@gmail.com>
2013-04-27 22:41:55 +0800

    add showdown and register markdown helper

--- js/app.js
+++ js/app.js
@@ -31,17 +31,23 @@ App.Post.FIXTURES = [{
   title      : "Hello Ember.JS",
   author     : "Sheng Loong",
   publishedAt: new Date('02-02-2013'),
-  intro      : "Ember.JS is a cool JS MVC framework",
-  extended   : "I love using Ember.JS"
+  intro      : "*Ember.JS* is a cool JS MVC framework",
+  extended   : "I love using *Ember.JS* (http://emberjs.com/)"
 }, {
   id         : 2,
   title      : "Hello Rails",
   author     : "Sheng Loong",
   publishedAt: new Date('05-04-2013'),
-  intro      : "Rails is a cool Ruby MVC framework",
-  extended   : "I love using Rails"
+  intro      : "*Rails* is a cool Ruby MVC framework",
+  extended   : "I love using [Rails] (http://rubyonrails.org/)"
 }];
 
 Ember.Handlebars.registerBoundHelper('date', function(date) {
   return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.registerBoundHelper('markdown', function(input) {
+  return new Ember.Handlebars.SafeString(showdown.makeHtml(input));
 });
