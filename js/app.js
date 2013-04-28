dad8fe0ca7c54632e469fd403e5ca5ac5005cb6a
Sheng-Loong Su <sushengloong@gmail.com>
2013-04-28 09:11:59 +0800

    redirect root to posts route

--- js/app.js
+++ js/app.js
@@ -12,6 +12,12 @@ App.Router.map(function() {
   this.resource('about');
 });
 
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('posts');
+  }
+});
+
 App.PostsRoute = Ember.Route.extend({
   model: function() {
     return App.Post.find(); 
